90eb4ac91249b1cb9c63762213d4a986cfd00879
joaomgsa <joaomgsa@gmail.com>
2021-10-11 23:49:21 -0300

    Primeiro exercicio da lista

---
+++ ExercicioAula3/src/exercicio1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner
+public class exercicio1 {
+	int[] valores= new int[5];// criação do arraycom a definição do tamanho do array
+	Scanner entrada = new Scanner(System.in); // criação da classe scanner
+	System.out.println("ArrayValores [5]\n");
+	for(int linha=0 ; linha < 5 ; linha++){
+	System.out.printf("Insira o elemento Valores [%d]: ", linha+1);
+	
+	int teste= entrada.nextInt(); 
+	if (teste%2!=0) {
+		System.out.println("Este vetor aceita somente numeros pares");
+	}
+	else {
+		valores[linha]=teste;
+	}
+		
+	// entrada de dados na matriz
+	}
+	System.out.println("\nOArrayficou: \n");
+	for(int linha=0 ; linha < 5 ; linha++){
+	System.out.printf("\t %d \t", valores[linha]);
+	}
+}
